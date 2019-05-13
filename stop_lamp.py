e2f775d75bfc5c81f51579a90dceb2d845c4e6bd
Matthias Richter <matthias.h.richter@gmail.com>
2019-05-13 08:55:49 -0700

    Remotely starts/stops Oriel Instruments lamp

---
+++ stop_lamp.py
@@ -0,0 +1,31 @@
+# Licence: GNU General Public License version 2 (GPLv2)
+# (C) 2019 Matthias H. Richter
+# remotely stops Oriel Instruments lamp
+import serial
+import sys
+import os
+import time
+
+serialport = 'COM1'
+
+if sys.platform == 'win32':
+	os.system('cls') # for windows users
+else:
+	os.system('clear') # for unix users
+
+
+# configure the serial connections
+ser = serial.Serial(
+    port=serialport,
+    baudrate=9600
+)
+ser.close()
+ser.open()
+if not ser.isOpen():
+  print('Serial port error')
+  sys.exit(1)
+  
+ser.write(str.encode('STOP\r\n'))
+time.sleep(1)
+
+ser.close()
